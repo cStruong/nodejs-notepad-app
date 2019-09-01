85a7bd3b2c947fdb1fb8f07158c92fec3c919010
Your Name <christophertruongm@gmail.com>
2019-08-31 20:22:24 -0400

    installed validator package and package.json

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const getNotes = require('./notes.js')
+const validator = require('validator');
+const getNotes = require('./notes.js');
 
 const msg = getNotes();
+
 console.log(msg)
+
+console.log(validator.isURL('https:/example.com'))
