657fda4c6012a8bf601cb469a478f1863b872560
Your Name <christophertruongm@gmail.com>
2019-09-01 17:45:50 -0400

    add functionality written

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
+yargs.version('1.1.0')
 
-console.log(msg)
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Post Title: ' + argv.title);
+        console.log('Post Body: ' + argv.body)
+    }
+})
 
-console.log(chalk.green.bold.inverse("Success!"));
+yargs.command({
+    command: 'remove',
+    describe: 'remove a new note',
+    handler: function () {
+        console.log('removing a new note...')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'list of all notes',
+    handler: function () {
+        console.log('listing all notes...')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function () {
+        console.log('reading a note...')
+    }
+})
+
+yargs.parse();
